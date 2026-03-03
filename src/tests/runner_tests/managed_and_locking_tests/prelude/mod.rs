ee65140313254f8dc8f971e1979b9590fc7a04df
Tom Wright <tom@inflatablecookie.com>
2026-03-03 15:20:39 +0000

    test: split managed and locking prelude helpers by concern

---
+++ src/tests/runner_tests/managed_and_locking_tests/prelude/mod.rs
@@ -0,0 +1,10 @@
+mod assertions;
+mod cases;
+mod fixtures;
+mod invocation_helpers;
+
+pub(super) use super::super::prelude::*;
+pub(crate) use assertions::*;
+pub(crate) use cases::*;
+pub(crate) use fixtures::*;
+pub(crate) use invocation_helpers::*;
